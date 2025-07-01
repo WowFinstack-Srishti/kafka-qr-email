3e26530d6cc8df26d8f7fd3085ad20bfbe0a725f
srishti-rao <srishtirao7@gmail.com>
2025-07-01 20:01:43 +0545

    Feature: Add EmailServiceImpl

--- src/main/java/com/wowfinstack/kafka_qr_email/service/impl/EmailServiceImpl.java
+++ src/main/java/com/wowfinstack/kafka_qr_email/service/impl/EmailServiceImpl.java
@@ -1,4 +1,31 @@
 package com.wowfinstack.kafka_qr_email.service.impl;
 
-public class EmailServiceImpl {
+import com.wowfinstack.kafka_qr_email.service.EmailService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @KafkaListener(topics = "email-notifications", groupId = "email-group")
+    public void listen(String email) {
+        sendQrGeneratedNotification(email);
+    }
+
+    @Override
+    public void sendQrGeneratedNotification(String to) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("QR Code Generated");
+        message.setText("Your QR code has been successfully generated.");
+        message.setFrom("no-reply@wowfinstack.com");
+        mailSender.send(message);
+    }
 }
