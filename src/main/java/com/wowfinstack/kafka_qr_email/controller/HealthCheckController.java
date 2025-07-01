bc2ea993840283e6f7108fbd9b2582f2f4ae6b54
srishti-rao <srishtirao7@gmail.com>
2025-07-01 20:03:06 +0545

    Feature: Add HealthCheckController

--- src/main/java/com/wowfinstack/kafka_qr_email/controller/HealthCheckController.java
+++ src/main/java/com/wowfinstack/kafka_qr_email/controller/HealthCheckController.java
@@ -1,4 +1,14 @@
 package com.wowfinstack.kafka_qr_email.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
 public class HealthCheckController {
+    @GetMapping
+    public String status() {
+        return "Kafka Email Service is up!";
+    }
 }
