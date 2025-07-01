057b87ba91373f71e5e41b03e204ae8892bc4e8b
srishti-rao <srishtirao7@gmail.com>
2025-07-01 18:14:29 +0545

    Initialize: Minimum Viable Product

---
+++ src/main/java/com/wowfinstack/kafka_qr_email/KafkaQrEmailApplication.java
@@ -0,0 +1,13 @@
+package com.wowfinstack.kafka_qr_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaQrEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaQrEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wowfinstack/kafka_qr_email/KafkaQrEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowfinstack.kafka_qr_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaQrEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
